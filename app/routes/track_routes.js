a65dd1a440f35a3ff61fd2416b7476996ca35114
Owen W. Krueger <okrueger14@gmail.com>
2018-03-07 22:22:50 -0500

    Added track routes

---
+++ app/routes/track_routes.js
@@ -0,0 +1,51 @@
+var ObjectID = require('mongodb').ObjectID;
+module.exports = function(app, db) {
+  app.get('/tracks/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('tracks').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      } 
+    });
+  });
+
+app.put('/tracks/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const track = { text: req.body.body, title: req.body.title };
+    db.collection('tracks').update(details, track, (err, result) => {
+      if (err) {
+          res.send({'error':'An error has occurred'});
+      } else {
+          res.send(track);
+      } 
+    });
+  }); 
+
+  app.delete('/tracks/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('tracks').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('track ' + id + ' deleted!');
+      } 
+    });
+  });
+
+app.post('/tracks', (req, res) => {
+    //const track = { text: req.body.body, title: req.body.title };
+    const track = { title: req.body.title, artists: req.body.artists, album: req.body.album};
+    db.collection('tracks').insert(track, (err, result) => {
+      if (err) { 
+        res.send({ 'error': 'An error has occurred' }); 
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+};
